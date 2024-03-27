173bda26e1f03341ea1970857d4f0a66a224326a
Santiago Izquierdo Varela <santiago.izquierdo2203@uppuebla.edu.mx>
2024-03-27 14:56:13 -0600

    Contralodor vercion 1

---
+++ app/Http/Controllers/CFDIController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class CFDIController extends Controller
+{
+    //
+    public function inicio()
+    {
+        return view('inicio');
+    }
+}
