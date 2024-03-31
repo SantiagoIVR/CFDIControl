8d298f0ce8efb690e56c145ab2fefb62b230a2d2
Santiago Izquierdo Varela <santiago.izquierdo2203@uppuebla.edu.mx>
2024-03-31 17:17:18 -0600

    .

--- resources/views/inicio.blade.php
+++ resources/views/inicio.blade.php
@@ -4,9 +4,7 @@
 @section('contenido')
         <center><h1> <b>Bienvenidos a CFDI </b></h1></center>
         <br><br>
-        <center><img class="rounded" src = "{{asset('archivos/logo.jpg')}}"
-        height = 400 width = 400></center>
+        <center><img class="rounded" src = "{{asset('archivos/logo.png')}}" height = 400 width = 400></center>
         <br></br>
-        
 @stop 
         
