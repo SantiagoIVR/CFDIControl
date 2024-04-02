5694215aae80a5a0ca8d8eed86b49ec4aae01a80
Santiago Izquierdo Varela <santiago.izquierdo2203@uppuebla.edu.mx>
2024-04-01 23:45:44 -0600

    Se agrego un session en el tiltulo

--- resources/views/inicio.blade.php
+++ resources/views/inicio.blade.php
@@ -2,9 +2,9 @@
 @extends('principal')
 
 @section('contenido')
-        <center><h1> <b>Bienvenidos a CFDI </b></h1></center>
+        <center><h1> <b>Bienvenidos a CFDI, {{ session('nombre') }}</b></h1></center>
         <br><br>
         <center><img class="rounded" src = "{{asset('archivos/logo.png')}}" height = 400 width = 400></center>
-        <br></br>
+        <br>
 @stop 
         
